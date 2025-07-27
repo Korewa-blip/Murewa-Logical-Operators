edf87ede07063257e0a5baa20a5a39f2b49959a8
Korewa-blip <murekosh@gmail.com>
2025-07-27 19:41:47 +0100

    Add files via upload

---
+++ Logical_Operators.py
@@ -0,0 +1,15 @@
+a=8
+b=3
+c=0
+if a and b and c:
+ print("all the members have boolean value as true")
+else:
+ print("atleast one member has got boolean value as false")
+if a or b or c:
+ print("atleast one memebr has got boolean value as false")
+else:
+ print("all the members have boolean value as false")
+if not c:
+ print("True")
+else:
+ print("False")
